c9b24dd00928183324cd4f321c5ccb6dd20057ab
Kirimi <215346793+okirimi@users.noreply.github.com>
2025-06-09 00:55:09 +0900

    feat: entry point for the bot

---
+++ src/suisei/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import os
+import signal
+from collections.abc import Generator
+from contextlib import contextmanager
+
+from src.suisei.cli import parse_args_and_setup_logging
+from src.suisei.discord.client import BotClient
+from src.suisei.discord.commands import *
+from src.suisei.discord.event import *
+from src.suisei.infrastructure.db.dao.access_dao import AccessLevelDAO
+
+
+@contextmanager
+def ignore_signals(signals: list[signal.Signals]) -> Generator[None, None, None]:
+    """Temporarily ignore specified signals.
+
+    Parameters
+    ----------
+    signals : list[signal.Signals]
+        A list of signals to ignore.
+
+    Examples
+    --------
+    >>> with ignore_signals([signal.SIGTERM, signal.SIGINT]):
+    ...     # processes whose signals are ignored
+    ...     pass
+    """
+    original_handlers = {sig: signal.getsignal(sig) for sig in signals}
+    try:
+        for sig in signals:
+            signal.signal(sig, signal.SIG_IGN)
+        yield
+    finally:
+        for sig, handler in original_handlers.items():
+            signal.signal(sig, handler)
+
+
+async def main() -> None:
+    """Entry point for the 'SuiseiBot'."""
+    logger = parse_args_and_setup_logging()
+
+    # Initialize database tables
+    await AccessLevelDAO().create_table()
+
+    # This system environment variable is specific to this function
+    DISCORD_BOT_TOKEN: str = os.environ["DISCORD_BOT_TOKEN"]  # noqa: N806
+
+    client = BotClient.get_instance()
+
+    try:
+        await client.start(DISCORD_BOT_TOKEN)
+    except Exception:
+        logger.exception("An unexpected error occurred")
+    finally:
+        with ignore_signals([signal.SIGTERM, signal.SIGINT]):
+            await client.cleanup_hook()
+            logger.info("Cleanup process finished")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
