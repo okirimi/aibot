a9212e07a41d98c89be7ab0c2fb68570ce8b06cc
Kirimi <215346793+okirimi@users.noreply.github.com>
2025-06-08 23:45:22 +0900

    refactor: privilegeをlevelという表現に変更

--- src/suisei/utils/decorators/access.py
+++ src/suisei/utils/decorators/access.py
@@ -4,10 +4,10 @@ from typing import TypeVar
 from discord import Interaction, app_commands
 
 from src.suisei.env import ADMIN_USER_IDS, AUTHORIZED_SERVER_IDS
-from src.suisei.infrastructure.db.dao.access_dao import AccessPrivilegeDAO
+from src.suisei.infrastructure.db.dao.access_dao import AccessLevelDAO
 
 _T = TypeVar("_T")
-access_dao: AccessPrivilegeDAO = AccessPrivilegeDAO()
+access_dao: AccessLevelDAO = AccessLevelDAO()
 
 
 def is_authorized_server() -> Callable[[_T], _T]:
@@ -27,7 +27,7 @@ def is_authorized_server() -> Callable[[_T], _T]:
 
 
 def is_admin_user() -> Callable[[_T], _T]:
-    """Check if the user has administrative access privilege.
+    """Check if the user has administrative access level.
 
     Returns
     -------
@@ -43,18 +43,18 @@ def is_admin_user() -> Callable[[_T], _T]:
 
 
 def is_advanced_user() -> Callable[[_T], _T]:
-    """Check if the user has advanced access privilege.
+    """Check if the user has advanced access level.
 
     Returns
     -------
     Callable[[_T], _T]
         A decorator that checks whether the user executing command is listed
-        in the table `access_privilege` with `advanced` access privilege.
+        in the table `access_level` with `advanced` access level.
     """
 
     async def predicate(interaction: Interaction) -> bool:
-        advanced_user_ids = await access_dao.fetch_user_ids_by_access_privilege(
-            access_privilege="advanced",
+        advanced_user_ids = await access_dao.fetch_user_ids_by_access_level(
+            access_level="advanced",
         )
         return interaction.user.id in advanced_user_ids
 
@@ -68,12 +68,12 @@ def is_not_blocked_user() -> Callable[[_T], _T]:
     -------
     Callable[[_T], _T]
         A decorator that checks whether the user executing command is not listed
-        in the table `blocked_user` with `blocked` access privilege.
+        in the table `blocked_user` with `blocked` access level.
     """
 
     async def predicate(interaction: Interaction) -> bool:
-        blocked_user_ids = await access_dao.fetch_user_ids_by_access_privilege(
-            access_privilege="blocked",
+        blocked_user_ids = await access_dao.fetch_user_ids_by_access_level(
+            access_level="blocked",
         )
         return interaction.user.id not in blocked_user_ids
 
